12443635c0989492781279eaaefd737020c167d2
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-05-14 13:22:44 +0200

    adding files to portfolio

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,23 +1,66 @@
-import React from 'react'
+// import React from 'react'
+// import './projectcard.css'
+// import Tilty from 'react-tilty'
+
+// const ProjectCard = ({image,title,alt, onClick}) => {
+
+
+
+//   return (
+//     <div className='card' onClick={onClick}>
+//         <Tilty className='tilt' scale={1.01} speed={200}>
+//             <div className='p__work_cards-card'>
+//                     <div className='p__work_cards-card_img'>
+//                         <img src={image} alt={`projekat ${alt}`} />
+//                     </div>
+//                     <div className='p__work_cards-card_info'>
+//                         <h3>{title}</h3>
+//                     </div>
+//             </div>
+//         </Tilty>
+//     </div>
+//   )
+// }
+
+// export default ProjectCard
+
+import React, {useEffect, useState} from 'react'
 import './projectcard.css'
 import Tilty from 'react-tilty'
 
-const ProjectCard = ({image,title,alt, onClick}) => {
+const ProjectCard = ({ image, title, alt, onClick }) => {
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
 
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 1024);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div className='card' onClick={onClick}>
+      {isMobile ? (
+        <div className='p__work_cards-card'>
+          <div className='p__work_cards-card_img'>
+            <img src={image} alt={`projekat ${alt}`} />
+          </div>
+          <div className='p__work_cards-card_info'>
+            <h3>{title}</h3>
+          </div>
+        </div>
+      ) : (
         <Tilty className='tilt' scale={1.01} speed={200}>
-            <div className='p__work_cards-card'>
-                    <div className='p__work_cards-card_img'>
-                        <img src={image} alt={`projekat ${alt}`} />
-                    </div>
-                    <div className='p__work_cards-card_info'>
-                        <h3>{title}</h3>
-                    </div>
+          <div className='p__work_cards-card'>
+            <div className='p__work_cards-card_img'>
+              <img src={image} alt={`projekat ${alt}`} />
             </div>
+            <div className='p__work_cards-card_info'>
+              <h3>{title}</h3>
+            </div>
+          </div>
         </Tilty>
+      )}
     </div>
   )
 }
