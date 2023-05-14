a33704e462120ac21f7ec07cce15f66037bc38ae
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-05-14 13:25:57 +0200

    adding files to portfolio

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import './projectcard.css'
-// import Tilty from 'react-tilty'
-
-// const ProjectCard = ({image,title,alt, onClick}) => {
-
-
-
-//   return (
-//     <div className='card' onClick={onClick}>
-//         <Tilty className='tilt' scale={1.01} speed={200}>
-//             <div className='p__work_cards-card'>
-//                     <div className='p__work_cards-card_img'>
-//                         <img src={image} alt={`projekat ${alt}`} />
-//                     </div>
-//                     <div className='p__work_cards-card_info'>
-//                         <h3>{title}</h3>
-//                     </div>
-//             </div>
-//         </Tilty>
-//     </div>
-//   )
-// }
-
-// export default ProjectCard
-
 import React, {useEffect, useState} from 'react'
 import './projectcard.css'
 import Tilty from 'react-tilty'
