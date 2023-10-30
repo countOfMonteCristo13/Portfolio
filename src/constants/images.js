cc5950995ca7b05110a25beb050e439e5fc24d20
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-10-30 22:35:52 +0100

    update profile img

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,55 +1,55 @@
-import openBracket from '../assets/openBracket.png'
-import closedBracket from '../assets/closedBracket.png'
-import closeYellow from '../assets/closeYellow.png'
-import closeGray from '../assets/closeGray.png'
-import me from '../assets/Nikola.jpg'
-import narutoPage from '../assets/narutoPage.png'
-import naruto from '../assets/projekatNaruto.png'
-import instagramLogo from '../assets/instagram.png'
-import githubLogo from '../assets/github.png'
-import emailLogo from '../assets/email.png'
-import linkedInLogo from '../assets/linkedin.png'
-import codepenLogo from '../assets/codepen.png'
-import discordLogo from '../assets/discord.png'
-import gamborini from '../assets/gamborini.png'
-import obodinac from '../assets/obodinac.png'
-import gamboriniPage from '../assets/gamboriniPage.png'
-import arrowBack from '../assets/arrowBack.png'
-import arrowBack1 from '../assets/arrowBack1.png'
-import obodinacPage from '../assets/obodinacPage.png'
-import magicBrain from '../assets/magicbrain.png'
-import magicBrainPage from '../assets/magicBrainPage.png'
-import check from '../assets/check.png'
-import up_arrow from '../assets/up-arrow.png'
-import velimirPorfolio from '../assets/velimir_portfolio.png'
-import velimirPorfolioPage from '../assets/velimirPortfolioPage.png'
+import openBracket from "../assets/openBracket.png";
+import closedBracket from "../assets/closedBracket.png";
+import closeYellow from "../assets/closeYellow.png";
+import closeGray from "../assets/closeGray.png";
+import me from "../assets/Nikola.png";
+import narutoPage from "../assets/narutoPage.png";
+import naruto from "../assets/projekatNaruto.png";
+import instagramLogo from "../assets/instagram.png";
+import githubLogo from "../assets/github.png";
+import emailLogo from "../assets/email.png";
+import linkedInLogo from "../assets/linkedin.png";
+import codepenLogo from "../assets/codepen.png";
+import discordLogo from "../assets/discord.png";
+import gamborini from "../assets/gamborini.png";
+import obodinac from "../assets/obodinac.png";
+import gamboriniPage from "../assets/gamboriniPage.png";
+import arrowBack from "../assets/arrowBack.png";
+import arrowBack1 from "../assets/arrowBack1.png";
+import obodinacPage from "../assets/obodinacPage.png";
+import magicBrain from "../assets/magicbrain.png";
+import magicBrainPage from "../assets/magicBrainPage.png";
+import check from "../assets/check.png";
+import up_arrow from "../assets/up-arrow.png";
+import velimirPorfolio from "../assets/velimir_portfolio.png";
+import velimirPorfolioPage from "../assets/velimirPortfolioPage.png";
 
 const images = {
-    openBracket,
-    closedBracket,
-    closeYellow,
-    closeGray,
-    me,
-    naruto,
-    narutoPage,
-    gamboriniPage,
-    instagramLogo,
-    githubLogo,
-    emailLogo,
-    linkedInLogo,
-    codepenLogo,
-    discordLogo,
-    gamborini,
-    obodinac,
-    arrowBack,
-    arrowBack1,
-    obodinacPage,
-    magicBrain,
-    magicBrainPage,
-    check,
-    up_arrow,
-    velimirPorfolio,
-    velimirPorfolioPage,
-}
+  openBracket,
+  closedBracket,
+  closeYellow,
+  closeGray,
+  me,
+  naruto,
+  narutoPage,
+  gamboriniPage,
+  instagramLogo,
+  githubLogo,
+  emailLogo,
+  linkedInLogo,
+  codepenLogo,
+  discordLogo,
+  gamborini,
+  obodinac,
+  arrowBack,
+  arrowBack1,
+  obodinacPage,
+  magicBrain,
+  magicBrainPage,
+  check,
+  up_arrow,
+  velimirPorfolio,
+  velimirPorfolioPage,
+};
 
 export default images;
